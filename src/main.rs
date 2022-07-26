129786139ce2f84a1a5473f1718f86a8513e76f6
imsamuka <imsamuka@gmail.com>
2022-07-26 15:47:55 -0300

    solved day 1

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ fn main() {
     use problems::*;
     for arg in std::env::args().skip(1) {
         let result: String = match arg.to_lowercase().trim_start_matches("day") {
-            "1a" => day1::problem_a().into(),
+            "1a" => day1::problem_a().to_string(),
+            "1b" => day1::problem_b().to_string(),
             _ => "unknown problem.".into(),
         };
         println!("{arg}: {result}");
--- src/problems/day1.rs
+++ src/problems/day1.rs
@@ -1,3 +1,20 @@
-pub fn problem_a() -> &'static str {
-    "unimplemented"
+use itertools::Itertools;
+
+fn problem_solution(n: usize) -> usize {
+    std::fs::read_to_string("res/day1.txt")
+        .unwrap()
+        .lines()
+        .filter_map(|l| l.parse().ok())
+        .combinations(n)
+        .find(|x| x.iter().sum::<usize>() == 2020)
+        .map(|x| x.iter().product())
+        .unwrap()
+}
+
+pub fn problem_a() -> usize {
+    problem_solution(2)
+}
+
+pub fn problem_b() -> usize {
+    problem_solution(3)
 }
